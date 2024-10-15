50b60fbb15a76d05bf8e55d070268ce09f3165d2
OmarAlanazi <oalanazi@villacapital.sa>
2024-10-15 13:48:18 +0300

    update moyasar sdk and package versions

--- example/app.config.js
+++ example/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "moyasar-apple-pay-example",
     slug: "moyasar-apple-pay-example",
-    version: "0.0.1",
+    version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
