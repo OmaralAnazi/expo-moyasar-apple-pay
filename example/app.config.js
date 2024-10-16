7de2f2386f540934f5aea18d49c1f8488d0c05ab
OmarAlanazi <oalanazi@villacapital.sa>
2024-10-16 14:04:51 +0300

    update package version

--- example/app.config.js
+++ example/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "moyasar-apple-pay-example",
     slug: "moyasar-apple-pay-example",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
